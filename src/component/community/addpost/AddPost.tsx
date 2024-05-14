aef91db2e8fe92e41a9531973a92d44a7aff7759
sunghyun1014 <sunghyun1014@naver.com>
2024-05-15 02:08:10 +0900

    커뮤니티 글쓰기 페이지 구현완료 및 반응형수정

--- src/component/community/addpost/AddPost.tsx
+++ src/component/community/addpost/AddPost.tsx
@@ -3,33 +3,94 @@ import React, { useRef, useState } from "react";
 import preview from "../../../assets/img/preview.jpg";
 import styled from "styled-components";
 import { SelectCategory } from "./SelectCategory";
+import axios from "../../../api/axios";
+
+import requests from "../../../api/requests";
+// import axios from "axios";
 
 interface selectImgType {
   id: string;
   img: string;
 }
+type requestImgType = {
+  img: string;
+};
+// interface requestObject {
+//   userId: number;
+//   communityId: number;
+//   communityType: string;
+//   title: string|undefined;
+//   text: string|undefined;
+// }
 
 export const AddPost = () => {
+  const [title, setTitle] = useState<string>();
+  const [text, setText] = useState<string>();
   const [mainCategory, setMainCategory] = useState<string>();
   const [subcategory, setSubCategory] = useState<string>();
   const [previmg, setPrevimg] = useState<selectImgType[] | undefined>();
   const [imgArray, setImgArray] = useState<selectImgType[]>([]);
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("게시글작성완료");
+    const customUserId = 515126;
+    let customCommunityType = "0";
+
+    switch (mainCategory) {
+      case "질문답변":
+        customCommunityType = "1";
+        break;
+      case "스터디":
+        customCommunityType = "2";
+        break;
+      default:
+        console.log("error");
+        break;
+    }
+    let communityObject = {
+      userId: customUserId,
+      communityId: 0,
+      communityType: customCommunityType,
+      title: title,
+      text: text,
+    };
+    const newImgArray = imgArray.map((item) => item.img);
+    const imageFiles = [...newImgArray];
+    console.log(title);
+    console.log(text);
+    console.log(mainCategory);
+    console.log(subcategory);
+    console.log(imageFiles);
+    await axios
+      .post(`${requests.community.addPost}`, {
+        communityObject,
+        imageFiles,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   const ref = useRef<HTMLDivElement>(null);
   let i = 0;
 
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
   const leftClick = () => {
     if (ref.current) {
-      ref.current.scrollLeft -= window.innerWidth - 80;
+      console.log(ref.current.scrollLeft);
+      ref.current.scrollLeft -= window.innerWidth - 10;
     }
   };
 
   const RightClick = () => {
     if (ref.current) {
-      ref.current.scrollLeft += window.innerWidth + 80;
+      ref.current.scrollLeft += window.innerWidth + 10;
     }
   };
 
@@ -61,94 +122,160 @@ export const AddPost = () => {
 
   return (
     <div className="relative">
-      <form onSubmit={onSubmit} encType="multipart/form-data">
+      <form
+        onSubmit={onSubmit}
+        encType="multipart/form-data"
+        className=" lg:max-w-[1200px]  mysm:w-[100%]
+        flex flex-col mysm:items-center lg:items-baseline
+        "
+      >
         {/* encType은 데이터를 백엔드로 보낼때 데이터들을 문자열이 아닌 여러형태의 자료(문자/바이너리)를 전송하기위한 전송 형태이다 */}
-        <header className="flex justify-between  flex-col">
-          <div className="flex lg:h-[100px]">
-            <h1>제목</h1>
-            <input type="text" className="border-[1px]" />
+        <header
+          className="flex justify-between md:items-center
+        md:flex-row mysm:flex-col
+        lg:max-w-[1150px] mysm:w-[100%]
+        pb-5
+        px-2"
+        >
+          <div className="flex  justify-between items-center md:m-0 mysm:mb-5">
+            <h1 className="mysm:font-semibold md:mr-2">제목</h1>
+            <input
+              type="text"
+              className="border-[1px] lg:w-[600px] md:w-[350px] mysm:w-[350px]
+              focus:outline-blue-500
+              rounded-md
+              lg:px-2 py-1
+              mysm:px-1 
+              "
+              placeholder="제목을 입력해주세요"
+              value={title}
+              onChange={handleChangeTitle}
+            />
           </div>
           <SelectCategory
             mainCategory={mainCategory}
             setMainCategory={setMainCategory}
+            subcategory={subcategory}
             setSubCategory={setSubCategory}
           ></SelectCategory>
         </header>
-        <textarea
-          name="overview"
-          id="overview"
-          className="resize-none border-[1px] lg:max-w-[1150px] mysm:w-[100vw]
-          h-[30dvh] outline-blue-500 p-5 rounded-lg overflow-x-hidden overflow-y-scroll"
-          placeholder="궁금한 점이나 취미들을 공유해주세요 &#13;
+        <main
+          className="flex flex-col justify-center lg:items-baseline mysm:items-center
+          mysm:w-[calc(100%-8px)]
+        "
+        >
+          <textarea
+            name="overview"
+            id="overview"
+            className="resize-none border-[1px] lg:max-w-[1150px] mysm:w-[calc(100%-16px)]
+          h-[30dvh] outline-blue-500 p-5 rounded-lg overflow-x-hiddenoverflow-y-scroll "
+            placeholder="궁금한 점이나 취미들을 공유해주세요 &#13;
             -글 작성 시 주의해야할 점
             -글 내용에 개인정보가 들어나면 안됩니다
             -개인정보가 포함된 글을 작성 후 문제발생시 모든책임은 글 작성자에게 있습니다
             -불건전한 게시글은 작성을 금지합니다
             "
-          maxLength={3000}
-        ></textarea>
-        <div className="flex relative">
-          <div className="w-10 z-10 absolute left-0" onClick={leftClick}>
-            <Left />
-          </div>
+            maxLength={3000}
+            value={text}
+            onChange={handleChangeText}
+          ></textarea>
           <div
-            className="flex overflow-hidden w-[600px] relative z-0"
-            ref={ref}
+            className="flex w-[calc(100%-16px)] justify-between
+          lg:flex-row
+          mysm:flex-col
+          "
           >
-            <div className="flex w-[600px] h-[300px]">
-              {imgArray?.map((item) => (
-                <div className="relative">
-                  <img
-                    key={item.id}
-                    src={item.img}
-                    alt=""
-                    id={item.id}
-                    onClick={modalOn}
-                    className="w-[150px] h-[150px] rounded-lg mx-1"
-                  />
-                  <button
-                    className="absolute top-0 right-3 text-red-600"
-                    onClick={modifyFile}
-                    id={item.id}
-                  >
-                    {"X"}
-                  </button>
+            <div className="flex relative justify-center lg:w-[600px] mysm:w-[100%] mt-5 ">
+              <div
+                className="w-10 z-10 absolute left-0  cursor-pointer h-[100%]
+            bg-[#B5B8BF]
+            flex justify-center items-center
+            rounded-lg
+            "
+                onClick={leftClick}
+              >
+                <Left />
+              </div>
+              <div
+                className="flex overflow-x-scroll overflow-y-hidden lg:w-[600px]  mysm:w-[100%] z-0 py-5  px-10 bg-gray-500/50 rounded-lg
+            shadow-[0px_8px_24px_rgba(149,157,165,0.3)]
+            "
+                ref={ref}
+              >
+                <div className="flex w-[600px] md:justify-normal mysm:justify-between ">
+                  {imgArray?.map((item) => (
+                    <div className="relative max-w-[150px] h-[150px] mx-3">
+                      <img
+                        key={item.id}
+                        src={item.img}
+                        alt="selectimg"
+                        id={item.id}
+                        onClick={modalOn}
+                        className="min-w-[150px] h-[150px] rounded-lg "
+                      />
+                      <button
+                        className="absolute top-0 right-2 text-red-600 font-extrabold"
+                        onClick={modifyFile}
+                        id={item.id}
+                      >
+                        {"X"}
+                      </button>
+                    </div>
+                  ))}
+                  <div>
+                    <input
+                      type="file"
+                      name=""
+                      id="imgAdd"
+                      multiple
+                      className="hidden"
+                      onChange={addFile}
+                      accept="image/*"
+                    />
+
+                    <label
+                      htmlFor="imgAdd"
+                      className={` justify-center items-center border-[1px] rounded-lg w-[150px] h-[150px] mx-1 bg-[#efefef]
+                    ${imgArray.length < 3 ? "flex" : "hidden"}
+                    `}
+                    >
+                      이미지 등록
+                    </label>
+                  </div>
                 </div>
-              ))}
-              <div>
-                <input
-                  type="file"
-                  name=""
-                  id="imgAdd"
-                  multiple
-                  className="hidden"
-                  onChange={addFile}
-                  accept="image/*"
-                />
-                {imgArray.length < 3 ? (
-                  <label
-                    htmlFor="imgAdd"
-                    className="flex justify-center items-center border-[1px] rounded-lg w-[150px] h-[150px] mx-1"
-                  >
-                    이미지 등록
-                  </label>
-                ) : (
-                  ""
-                )}
+              </div>
+              <div
+                className="w-10 z-10 absolute right-0  cursor-pointer flex h-[100%] items-center justify-center
+            bg-[#B5B8BF] rounded-lg
+            "
+                onClick={RightClick}
+              >
+                <Right />
               </div>
             </div>
+            <div
+              className="mt-5 flex items-end
+            mysm:justify-end
+            
+            "
+            >
+              <input type="submit" value="" id="submitBtn" />
+              <label
+                htmlFor="submitBtn"
+                className=" border-[1px] px-3 py-2  rounded-md text-white bg-[#3B82F6] cursor-pointer"
+              >
+                작성하기
+              </label>
+            </div>
           </div>
-          <div className="w-10 z-10 absolute right-0" onClick={RightClick}>
-            <Right />
-          </div>
-        </div>
-
+        </main>
         {previmg && (
           <div
-            className={`absolute -top-5 z-10 max-w-[100vw] lg:h-[calc(100dvh-185px)] mysm:h-[calc(100dvh-172px)] flex flex-col justify-center items-center bg-[#333B3D]/90 backdrop-blur-sm`}
+            className={`absolute -top-5 lg:-left-5 z-10 lg:max-w-[1200px] mysm:w-[100vw] lg:h-[calc(100dvh-185px)] mysm:h-[calc(100dvh-192px)] flex flex-col justify-center items-center bg-[#333B3D]/90 
+            backdrop-blur-sm`}
           >
             <div className="relative">
-              <img src={previmg[0].img} alt="previmg" className="px-1" />
+              <img src={previmg[0].img} alt="previmg" className="px-2" />
               <button
                 className="absolute top-[1%] right-[1%] text-white bg-[#1F38A1] w-20 h-8 rounded-lg"
                 onClick={() => setPrevimg(undefined)}
@@ -165,12 +292,12 @@ export const AddPost = () => {
 
 const Left = () => {
   return (
-    <div>
+    <div className="w-10">
       <svg
         data-slot="icon"
         fill="none"
         stroke-width="1.5"
-        stroke="#FFD7DE"
+        stroke="#5c6177"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
@@ -187,12 +314,12 @@ const Left = () => {
 
 const Right = () => {
   return (
-    <div>
+    <div className="w-10">
       <svg
         data-slot="icon"
         fill="none"
         stroke-width="1.5"
-        stroke="#FFD7DE"
+        stroke="#5c6177"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
--- src/component/community/addpost/SelectCategory.tsx
+++ src/component/community/addpost/SelectCategory.tsx
@@ -3,18 +3,18 @@ import React from "react";
 interface ICommuCategory {
   mainCategory: string | undefined;
   setMainCategory: (mainCategory: string) => void;
-
+  subcategory: string | undefined;
   setSubCategory: (subCategory: string) => void;
 }
 
 export const SelectCategory = ({
   mainCategory,
   setMainCategory,
-
+  subcategory,
   setSubCategory,
 }: ICommuCategory) => {
   const changeSubCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const data = e.currentTarget.id;
+    const data = e.currentTarget.value;
     setSubCategory(data);
   };
   const changeMainCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -22,34 +22,36 @@ export const SelectCategory = ({
     setMainCategory(data);
   };
   return (
-    <div className=" z-10 overflow-hidden flex ">
-      <div>
+    <div className=" z-10 overflow-hidden flex mx-1">
+      <div className="flex">
+        <h1 className="font-semibold">카테고리</h1>
         <select
           name=""
           id=""
-          value={mainCategory}
+          value={mainCategory === null ? "1" : mainCategory}
           onChange={changeMainCategory}
-          className="border-[1px] rounded-md"
+          className="border-[1px] rounded-md mysm:ml-3"
         >
-          <option value="">---카테고리---</option>
+          <option value="1">---카테고리---</option>
           <option value="질문답변">질문답변</option>
           <option value="스터디">스터디</option>
         </select>
       </div>
 
       <div
-        className={`ml-3 z-0 translate-x-[100px] transition-transform ${
-          mainCategory && "translate-x-[-0%]"
+        className={`ml-3 z-0 translate-x-[100%] transition-transform ${
+          mainCategory && "translate-x-[0%]"
         } `}
       >
         {mainCategory && mainCategory === "질문답변" && (
           <select
             name=""
             id=""
-            value={mainCategory}
+            value={subcategory && subcategory}
             onChange={changeSubCategory}
             className="border-[1px] rounded-md"
           >
+            <option value="1">---카테고리---</option>
             <option value="공부">공부</option>
             <option value="질문답변">고민</option>
             <option value="스터디">취미</option>
@@ -60,12 +62,15 @@ export const SelectCategory = ({
           <select
             name=""
             id=""
-            value={mainCategory}
+            value={subcategory}
             onChange={changeSubCategory}
             className="border-[1px] rounded-md"
           >
+            <option value="1">---카테고리---</option>
             <option value="모집중">모집중</option>
-            <option value="모집완료">모집완료</option>
+            <option value="모집완료" disabled>
+              모집완료
+            </option>
           </select>
         )}
       </div>
