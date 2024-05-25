4966895b04f1357c08c0ed16f20ccb8253792d37
sunghyun1014 <sunghyun1014@naver.com>
2024-05-25 22:25:11 +0900

    커뮤니티 헤더 텍스트 수정

--- src/component/community/CommunityHeader.tsx
+++ src/component/community/CommunityHeader.tsx
@@ -9,7 +9,9 @@ export const CommunityHeader = () => {
       <div className=" mysm:max-w-[100vw] lg:max-w-[1200px]  flex justify-start items-center py-3 lg:px-5 mx-auto my-0 h-[100px]">
         <h1 className="text-white/90 font-bold text-2xl   lg:py-1 lg:px-0 md:pl-5 mysm:pl-3 mysm:py-2">
           커뮤니티 페이지
-          <p className="py-2 text-sm">다양한 의견을 나눠봐요</p>
+          <p className="py-2 text-sm">
+            다양한 의견을 나눠보고, 스터디도 구해봐요~
+          </p>
         </h1>
       </div>
     </div>
