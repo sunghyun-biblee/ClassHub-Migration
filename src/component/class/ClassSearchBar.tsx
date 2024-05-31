db08d2146acadd9cd3fc86b391006212344cdd6f
sunghyun1014 <sunghyun1014@naver.com>
2024-06-01 00:07:16 +0900

    강의 검색기능 추가

--- src/component/class/ClassSearchBar.tsx
+++ src/component/class/ClassSearchBar.tsx
@@ -1,8 +1,14 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { fetchsearchKeyWord } from "component/navigtaion/hooks/fetchsearchKeyWord";
 import { useClassCategory } from "hooks/ClassTypeProvider";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export const ClassSearchBar = () => {
+  const [searchKeyWord, setSearchKeyWord] = useState("");
+  const queryClient = useQueryClient();
+  const nav = useNavigate();
   const classType = useClassCategory();
   const renderCategory = () => {
     switch (classType.classCategoryType) {
@@ -20,20 +26,30 @@ export const ClassSearchBar = () => {
         return "오류";
     }
   };
+  const handleSubmitSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    nav(`/class/${searchKeyWord}`);
+    queryClient.prefetchQuery({
+      queryKey: ["searchList"],
+      queryFn: () => fetchsearchKeyWord(searchKeyWord),
+    });
+  };
   return (
     <div className="py-2">
       <div className="flex justify-between  items-center">
         <h1 className="font-extrabold text-lg">{renderCategory()}</h1>
-        <div className="flex">
+        <form className="flex" onSubmit={handleSubmitSearch}>
           <Input
             type="text"
             placeholder="강의 검색"
+            value={searchKeyWord}
             className="pl-2 font-semibold"
+            onChange={(e) => setSearchKeyWord(e.target.value)}
           />
           <div className="p-1 bg-[#F1F3F5] border-solid border-[1px] border-[#000000]/30 rounded-r-[8px] font-semibold">
             <button className="text-[14px] px-2 py-[2px]">검색</button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
