6330d2361aa1accd4d26fd43bf9e8fc64eae8200
sunghyun1014 <sunghyun1014@naver.com>
2024-07-27 00:19:47 +0900

    Refactor: 구글로그인을 firebase 로그인으로 변경
    마이그레이션을 위해 서버와 연결하지않고 프론트쪽에서 firebase를 통해
    구글로그인  변경

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "../node_modules/firebase/app";
+
+// import { getAnalytics } from "firebase/analytics";
+
+import { getFirestore } from "../node_modules/firebase/firestore";
+import { getAuth } from "../node_modules/firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +21,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+// const analytics = getAnalytics(fireBaseApp);
